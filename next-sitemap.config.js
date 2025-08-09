8135f442f3cf24a5f1efa5fa7f39a422bde5e642
Alex Chesnay <126405110+Alexmennechet@users.noreply.github.com>
2025-08-09 04:32:33 -0700

    Include privacy policy in sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,7 @@
 export default {
   siteUrl: 'https://alex-chesnay.com',
   generateRobotsTxt: true,
+  additionalPaths: async (config) => [
+    await config.transform(config, '/politique-de-confidentialite'),
+  ],
 };
